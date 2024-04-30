6a3fa7a975c7fef6000f07b1b2d958443de9ec5e
FasorUB <113418120+FasorUB@users.noreply.github.com>
2024-04-30 22:03:36 +0200

    Create base_tests.js

---
+++ js/base_tests.js
@@ -0,0 +1,43 @@
+let base_test = [
+    {
+        //Si quieres quitar la pista en alguna eliminar la linia de pista
+        pregunta: "¿Que mano de las siguientes gana contra una escalera?",
+        pista: "Recuerda la jerarquia",
+        pista_img: "../img/Jerarquia.jpg",
+        imagen: "../img/Test1.png",
+        respuesta: "Color/flush",
+        distractores:["Carta Alta A","Doble pareja de A-A, K-K","Trío de A's"],
+    },
+    {
+        pregunta: "¿Que mano es la más alta en póker (si jugamos sin comodines)?",
+        pista: "Recuerda la jerarquia",
+        pista_img: "../img/Jerarquia.jpg",
+        imagen: "../img/Test2.jpg",
+        respuesta: "Escalera real de color",
+        distractores:["Poker de A's","Full-House","Escalera de A-10"],
+    },
+    {
+        pregunta: "¿En que turnos se divide una partida de poker?",
+        pista: "Recuerda que las 3 primeras cartas se obtienen en el floop y la última en el River",
+        pista_img: "../img/Jerarquia.jpg",
+        imagen: "../img/Test3.jpg",
+        respuesta: "Floop-Turn-River",
+        distractores:["Turn-Floop-River","River-Turn-Floop","Floop-River-Turn"],
+    },
+    {
+        pregunta: "¿Que mano de inicio es la mejor (probabílisticamente hablando)?",
+        pista: "Recuerda que el As es la carta jerarquicamente más alta",
+        pista_img: "../img/Tabla_preflop.png",
+        imagen: "../img/Test4.jpg",
+        respuesta: "Pareja de Ases",
+        distractores:["Pareja de K's","2-7 offsuit","J-10 suited"],
+    },
+    {
+        pregunta: "¿Que es un bluff?",
+        pista: "Recuerda que en el poker esta permitido mentir!",
+        pista_img: "../img/Tabla_preflop.png",
+        imagen: "../img/Test6.jpg",
+        respuesta: "Hacer ver que tienes una mano mejor que la que realmente tienes y subir la apuesta",
+        distractores:["Hacer check","Foldear las cartas","Igualar la apuesta"],
+    },
+];
